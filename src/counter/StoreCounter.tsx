754abf1be852abfc84c48a034f33298df7990a20
formegusto <formegusto@gmail.com>
2020-11-20 10:08:19 +0900

    Store Mobx Init

--- src/counter/StoreCounter.tsx
+++ src/counter/StoreCounter.tsx
@@ -1,17 +1,22 @@
 import { inject, observer } from 'mobx-react';
 import React from 'react';
+import CounterStore from '../stores/CounterStore';
+
+interface Props {
+    counter?:  CounterStore
+}
 
 @inject('counter')
 @observer
-class StoreCounter extends React.Component {
+class StoreCounter extends React.Component<Props> {
+    counter = this.props.counter
     render() {
-        const { counter } : any = this.props;
         return (
             <div>
                 <h1>It is Store Mobx Component</h1>
-                <h1>{counter.number}</h1>
-                <button onClick={(e) => counter.increase()}>+1</button>
-                <button onClick={(e) => counter.decrease()}>-1</button>
+                <h1>{this.counter!.number}</h1>
+                <button onClick={(e) => this.counter!.increase()}>+1</button>
+                <button onClick={(e) => this.counter!.decrease()}>-1</button>
             </div>
         );
     }
